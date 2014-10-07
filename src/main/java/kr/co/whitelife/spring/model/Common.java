84e66e26ddefdd1806bb28a605ba9c0a1ad6d41b
whitelife <parkmc_0@naver.com>
2014-10-08 00:26:09 +0900

    commit

--- src/main/java/kr/co/whitelife/spring/model/Common.java
+++ src/main/java/kr/co/whitelife/spring/model/Common.java
@@ -187,42 +187,42 @@ public class Common {
 	 */
 	private void makePaging() {
 		if (this.totalCount == 0) return; // 게시 글 전체 수가 없는 경우
-        if (this.pageNo == 0) this.setPageNo(1); // 기본 값 설정
-        if (this.pageSize == 0) this.setPageSize(10); // 기본 값 설정
+		if (this.pageNo == 0) this.setPageNo(1); // 기본 값 설정
+		if (this.pageSize == 0) this.setPageSize(10); // 기본 값 설정
 
-        int finalPage = (totalCount + (pageSize - 1)) / pageSize; // 마지막 페이지
-        if (this.pageNo > finalPage) this.setPageNo(finalPage); // 기본 값 설정
+		int finalPage = (totalCount + (pageSize - 1)) / pageSize; // 마지막 페이지
+		if (this.pageNo > finalPage) this.setPageNo(finalPage); // 기본 값 설정
 
-        if (this.pageNo < 0 || this.pageNo > finalPage) this.pageNo = 1; // 현재 페이지 유효성 체크
+		if (this.pageNo < 0 || this.pageNo > finalPage) this.pageNo = 1; // 현재 페이지 유효성 체크
 
-        boolean isNowFirst = pageNo == 1 ? true : false; // 시작 페이지 (전체)
-        boolean isNowFinal = pageNo == finalPage ? true : false; // 마지막 페이지 (전체)
+		boolean isNowFirst = pageNo == 1 ? true : false; // 시작 페이지 (전체)
+		boolean isNowFinal = pageNo == finalPage ? true : false; // 마지막 페이지 (전체)
 
-        int startPage = ((pageNo - 1) / 10) * 10 + 1; // 시작 페이지 (페이징 네비 기준)
-        int endPage = startPage + 10 - 1; // 끝 페이지 (페이징 네비 기준)
+		int startPage = ((pageNo - 1) / 10) * 10 + 1; // 시작 페이지 (페이징 네비 기준)
+		int endPage = startPage + 10 - 1; // 끝 페이지 (페이징 네비 기준)
 
-        if (endPage > finalPage) { // [마지막 페이지 (페이징 네비 기준) > 마지막 페이지] 보다 큰 경우
-            endPage = finalPage;
-        }
+		if (endPage > finalPage) { // [마지막 페이지 (페이징 네비 기준) > 마지막 페이지] 보다 큰 경우
+			endPage = finalPage;
+		}
 
-        this.setFirstPageNo(1); // 첫 번째 페이지 번호
+		this.setFirstPageNo(1); // 첫 번째 페이지 번호
 
-        if (isNowFirst) {
-            this.setPrevPageNo(1); // 이전 페이지 번호
-        } else {
-            this.setPrevPageNo(((pageNo - 1) < 1 ? 1 : (pageNo - 1))); // 이전 페이지 번호
-        }
+		if (isNowFirst) {
+			this.setPrevPageNo(1); // 이전 페이지 번호
+		} else {
+			this.setPrevPageNo(((pageNo - 1) < 1 ? 1 : (pageNo - 1))); // 이전 페이지 번호
+		}
 
-        this.setStartPageNo(startPage); // 시작 페이지 (페이징 네비 기준)
-        this.setEndPageNo(endPage); // 끝 페이지 (페이징 네비 기준)
+		this.setStartPageNo(startPage); // 시작 페이지 (페이징 네비 기준)
+		this.setEndPageNo(endPage); // 끝 페이지 (페이징 네비 기준)
 
-        if (isNowFinal) {
-            this.setNextPageNo(finalPage); // 다음 페이지 번호
-        } else {
-            this.setNextPageNo(((pageNo + 1) > finalPage ? finalPage : (pageNo + 1))); // 다음 페이지 번호
-        }
+		if (isNowFinal) {
+			this.setNextPageNo(finalPage); // 다음 페이지 번호
+		} else {
+			this.setNextPageNo(((pageNo + 1) > finalPage ? finalPage : (pageNo + 1))); // 다음 페이지 번호
+		}
 
-        this.setFinalPageNo(finalPage); // 마지막 페이지 번호
+		this.setFinalPageNo(finalPage); // 마지막 페이지 번호
 	}
 
 	@Override
