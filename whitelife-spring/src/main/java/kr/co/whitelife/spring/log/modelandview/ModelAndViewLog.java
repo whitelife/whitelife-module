7994ff063db7a94984dfb0bb5bb727b3a33a1188
whitelife <parkmc_0@naver.com>
2014-10-13 17:46:45 +0900

    ModelAndViewLog Update

--- whitelife-spring/src/main/java/kr/co/whitelife/spring/log/modelandview/ModelAndViewLog.java
+++ whitelife-spring/src/main/java/kr/co/whitelife/spring/log/modelandview/ModelAndViewLog.java
@@ -4,18 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import kr.co.whitelife.spring.log.Log;
 
 import org.slf4j.Logger;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * ModelAndViewLog
  *
+ * <pre>
+ * Ajax Log 기능 추가, Controller Return Type 별 Log 기능 추가
+ * </pre>
+ *
  * @author whitelife
- * @since 2014.10.10
- * @version 0.1
+ * @since 2014.10.13
+ * @version 0.2
  */
 public class ModelAndViewLog implements Log {
 
@@ -25,56 +33,58 @@ public class ModelAndViewLog implements Log {
 
 	@SuppressWarnings("unchecked")
 	public void resultLog(Object result, Logger logger, Object[] args) {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		boolean isAjax = "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
+
 		if (result == null) return;
 		if (logger == null) return;
 
-		if (result instanceof ModelAndView) {
-			this.modelAndViewLog((ModelAndView) result, logger);
-		}
-
-		if (result instanceof String) {
-			ModelMap modelMap = null;
-
-			for (Object obj : args) {
-				if (obj instanceof ModelMap) {
-					modelMap = (ModelMap) obj;
-					break;
-				}
+		if (!isAjax) {
+			if (result instanceof ModelAndView) {
+				this.modelAndViewLog((ModelAndView) result, logger);
 			}
 
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName(String.valueOf(result));
-			mav.addAllObjects(modelMap);
+			if (result instanceof String) {
+				ModelMap modelMap = null;
 
-			this.modelAndViewLog(mav, logger);
+				for (Object obj : args) {
+					if (obj instanceof ModelMap) {
+						modelMap = (ModelMap) obj;
+						break;
+					}
+				}
+
+				ModelAndView mav = new ModelAndView();
+				mav.setViewName(String.valueOf(result));
+				mav.addAllObjects(modelMap);
+
+				this.modelAndViewLog(mav, logger);
+			}
+
+			if (result instanceof Map) {
+				ModelAndView mav = new ModelAndView();
+
+				ModelMap modelMap = new ModelMap();
+				modelMap.addAllAttributes((Map<String, ?>) result);
+
+				mav.addAllObjects(modelMap);
+
+				this.modelAndViewLog(mav, logger);
+			}
 		}
 
-		if (result instanceof Map) {
-			ModelAndView mav = new ModelAndView();
-
-			ModelMap modelMap = new ModelMap();
-			modelMap.addAllAttributes((Map<String, ?>) result);
-
-			mav.addAllObjects(modelMap);
-
-			this.modelAndViewLog(mav, logger);
+		if (isAjax) {
+			this.resultAjaxLog(result, logger, args);
 		}
 	}
 
 	/**
-	 * modelAndView Logger
+	 * model Logger
 	 *
-	 * @param mav ModelAndView
-	 * @param logger Logger
+	 * @param model Model
+	 * @param report StringBuffer
 	 */
-	private void modelAndViewLog(ModelAndView mav, Logger logger) {
-		Map<String, Object> model = mav.getModel();
-		StringBuffer report = new StringBuffer();
-
-		report.append("\n").append("-----------------------------------------ModelAndView Information----------------------------------------");
-		report.append("\n").append("view        : ").append(mav.getView() == null ? "" : mav.getView().toString());
-		report.append("\n").append("viewname    : ").append(mav.getViewName() == null ? "" : mav.getViewName());
-
+	private void modelLog(Map<String, Object> model, StringBuffer report) {
 		if (model != null) {
 			boolean isFirst = true;
 
@@ -98,6 +108,56 @@ public class ModelAndViewLog implements Log {
 				report.append("\n]");
 			}
 		}
+	}
+
+	/**
+	 * ajax Logger
+	 *
+	 * @param result 결과 값
+	 * @param logger object
+	 * @param args object[]
+	 */
+	private void resultAjaxLog(Object result, Logger logger, Object[] args) {
+		StringBuffer report = new StringBuffer();
+
+		report.append("\n").append("-----------------------------------------ModelAndView Information----------------------------------------");
+		report.append("\n").append("ajax        : ").append("true");
+
+		if (result instanceof ModelAndView) {
+			this.modelLog(((ModelAndView) result).getModel(), report);
+		}
+
+		if (result instanceof String) {
+			report.append("\n").append("json        : ").append(result == null ? "" : result);
+		}
+
+		if (result instanceof Map) {
+			report.append("\n").append("json        : ").append(result == null ? "" : result.toString());
+		}
+
+		report.append("\n").append("---------------------------------------------------------------------------------------------------------");
+
+		if (logger.isDebugEnabled()) {
+			logger.debug(report.toString());
+		}
+	}
+
+	/**
+	 * modelAndView Logger
+	 *
+	 * @param mav ModelAndView
+	 * @param logger Logger
+	 */
+	private void modelAndViewLog(ModelAndView mav, Logger logger) {
+		Map<String, Object> model = mav.getModel();
+		StringBuffer report = new StringBuffer();
+
+		report.append("\n").append("-----------------------------------------ModelAndView Information----------------------------------------");
+		report.append("\n").append("ajax        : ").append("false");
+		report.append("\n").append("view        : ").append(mav.getView() == null ? "" : mav.getView().toString());
+		report.append("\n").append("viewname    : ").append(mav.getViewName() == null ? "" : mav.getViewName());
+
+		this.modelLog(model, report);
 
 		report.append("\n").append("---------------------------------------------------------------------------------------------------------");
 
